74e0f3ff51e0fe2f62d9a0ffed363e3542bd0b0d
ikilllliki <rinka_16n1200079@nnn.ed.jp>
2017-07-03 15:48:42 +0900

    リスト6-17を反映

--- text.js
+++ text.js
@@ -4,3 +4,19 @@ document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('result_html').innerHTML =
     '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
 },false);
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    var list = document.getElementById('list');
+    console.log(list.innerHTML);
+    console.log(list.textContent);
+},false);
+
+
+document.addEventListener('DOMContentLoaded',function() {
+    document.getElementById('btn').addEventListener('click',function() {
+        var name = document.getElementById('name');
+        var result = document.getElementById('result');
+        result.innerHTML = 'こんにちは、' + name.value + 'さん！ ';
+    },false);
+},false);
