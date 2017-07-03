ebd77a934ada64507ba08a9ccaa914d509b9e1af
ikilllliki <rinka_16n1200079@nnn.ed.jp>
2017-07-03 15:01:49 +0900

    リスト6-16

--- text.js
+++ text.js
@@ -4,3 +4,10 @@ document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('result_html').innerHTML =
     '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
 },false);
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    let list = document.getElementById('list');
+    console.log(list.innerHTML);
+    console.log(list.textContent);
+},false);
